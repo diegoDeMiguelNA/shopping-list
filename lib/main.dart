4447add77510b872493581ac93234a421f0c0489
Progress with the component <deMiguel@newsaktuell.de>
2023-06-29 14:21:23 +0200

    improved architecture of the app. using list as dynamic values

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/screens/groceries.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: const Groceries(),
+      home: const GroceryList(),
     );
   }
 }
