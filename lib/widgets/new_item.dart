bd2e435eea35ed929f61928278460000e55cebab
Progress with the component <deMiguel@newsaktuell.de>
2023-07-05 08:55:58 +0200

    Implemented Secrets on the repository

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,12 +21,12 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      final url = Uri.https('flutter-prep-f05ca-default-rtdb.firebaseio.com',
-          'shopping_list.json');
-      http.post(
+      final dBUrl = Uri.parse(dotenv.env['API_URL']!).toString();
+      final url = Uri.https(dBUrl, 'shopping_list.json');
+      final response = http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -35,13 +37,13 @@ class _NewItemState extends State<NewItem> {
           'category': _selectedCategory.title,
         }),
       );
-      // Navigator.of(context).pop(
-      //   GroceryItem(
-      //       id: DateTime.now().toString(),
-      //       name: _enteredName,
-      //       quantity: _enteredQuantity,
-      //       category: _selectedCategory),
-      // );
+      print(response);
+      print(response);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
